34f22ef4e78e516e6ca19d58488a027132190ff8
dlawhdgus <212whdgus@gmail.com>
2022-10-07 13:39:26 +0900

    add DBconnect

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 2008
+const config = require('./config')
+const { DBConnect } = require('./DBconnect')
+
 const routes = require('./apis/index')
 
 app.use(express.json())
-app.use(routes)
+app.use('/api',routes)
 
-app.listen(port, () => console.log(`Server On!!`))
+app.all('/*',(req,res) => {
+    res.status(404).send('Not Found')
+})
+
+app.listen(config.port, () => {
+    console.log(`Server On!!
+port number is 2008`)
+    DBConnect()
+})
