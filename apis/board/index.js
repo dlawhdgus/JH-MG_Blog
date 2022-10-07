b9feaa13e4874c5a980b135e8634025463e6d66c
dlawhdgus <212whdgus@gmail.com>
2022-10-07 13:40:07 +0900

    show detail

--- apis/board/index.js
+++ apis/board/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const controller = require('./controller')
 
-router.post('/', controller.CreateArticleCode)
+router
+    .post('/', controller.CreateArticleCode)
     .get('/', controller.ReadArticleAllCode)
     .get('/:id', controller.ReadArticleIdCode)
     .patch('/:id', controller.UpdateArticleCode)
-
+    .delete('/:id', controller.DeleteOneArticleCode)
 module.exports = router
